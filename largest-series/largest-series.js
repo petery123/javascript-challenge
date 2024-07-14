705e201ef2b1ce769b1471cb9099193227e4766a
petery123 <139976625+petery123@users.noreply.github.com>
2024-07-14 22:00:20 +0100

    Add update display fuctionality
    -Make largestData object to store all data of largest multiplication

--- largest-series/largest-series.js
+++ largest-series/largest-series.js
@@ -3,6 +3,10 @@ const integerList = document.querySelector("#integerList");
 const numOfSeries = document.querySelector("#numOfSeries");
 const result = document.querySelector("#result");
 
+const largestResult = document.querySelector("#largestResult");
+const largestListResult = document.querySelector("#largestListResult");
+const largestPositionResult = document.querySelector("#largestPositionResult");
+
 checkResultBtn.addEventListener("click", solve);
 
 function solve(){
@@ -10,9 +14,12 @@ function solve(){
     const numList = integerList.value;
     if (validInput(num, numList)){
         let arr = numList.split("").map(((element) => {return +element}));
-        let startIndex = 0;
-        let largest = 0;
-        let arrOfLargest = arr.slice(0, num); //if highest ends up being 0 will be the first set
+        let largestData = {
+            largest : 0,
+            list : arr.slice(0, num),
+            startPositon : 0,
+        };
+         //if highest ends up being 0 will be the first set
         const repeatLen = arr.length-(num);
         for (i = 0; i <= repeatLen; i++){
             let hold_arr = arr.slice(i, (i+num));
@@ -20,17 +27,16 @@ function solve(){
                 i += hold_arr.indexOf(0);
                 continue;
             }
-            if (multiply(hold_arr) > largest){
-                largest = multiply(hold_arr);
-                arrOfLargest = hold_arr;
-                startPositon = i;
+            if (multiply(hold_arr) > largestData.largest){
+                largestData.largest = multiply(hold_arr);
+                largestData.list = hold_arr;
+                largestData.startPositon = i;
             }
-            console.log(largest);
-            console.log(arrOfLargest);
-            console.log(startPositon);
+            updateDisplay(largestData.largest, largestData.list, largestData.startPositon);
         }
+    }else{
+        alert("Invalid Input!")
     }
-    return "Ivalid Input";
 };
 
 function validInput(num, arr){
@@ -45,4 +51,10 @@ function validInput(num, arr){
 
 function multiply(arr){
     return arr.reduce((multiplied, current) => multiplied * current);
+}
+
+function updateDisplay(largest, arrOfLargest, startPositon){
+    largestResult.textContent = largest;
+    largestListResult.textContent = arrOfLargest;
+    largestPositionResult.textContent = startPositon;
 }
