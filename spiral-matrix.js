c5b0f06b2b201c2656508ca0b8be95515a018f2c
petery123 <139976625+petery123@users.noreply.github.com>
2024-07-15 18:48:52 +0100

    Add feature to make first row of spiral

--- spiral-matrix.js
+++ spiral-matrix.js
@@ -1,11 +1,31 @@
-const MOVES = [
-    [1,0], //right
-    [0,1], //down
-    [-1,0], //left
-    [0,-1] //up
-]
-
 const matrixSize = document.querySelector("#matrixSize");
+const getSpiralBtn = document.querySelector("#getSpiralBtn");
+const result = document.querySelector(".result")
+
+getSpiralBtn.addEventListener("click", updateResult);
+
+let movement = {
+    moves: [
+        [0, 1], //right
+        [1, 0], //down
+        [0,-1], //left
+        [-1,0], //up
+    ],
+    moveIndex: 0,
+    currentMove: [0, 1],
+    nextMove: function(){
+        if (this.moveIndex === 3){
+            this.moveIndex = 0;
+        }else{
+            this.moveIndex++;
+        }
+        this.currentMove = this.moves[this.moveIndex]
+    }
+}
+let currentPos = {
+    x: 0,
+    y: 0
+};
 
 function createEmptyMatrix(length){
     let emptyMatrix = [];
@@ -19,4 +39,42 @@ function createEmptyMatrix(length){
     return emptyMatrix;
 }
 
-console.log(createEmptyMatrix(5));
+function createSpiralMatrix(length){
+    if (validLength(length)){
+        matrix = createEmptyMatrix(length);
+        fillFirstRow(length);
+        
+        console.log(matrix);
+        console.log(movement);
+        return matrix;
+    }
+    return false;
+}
+
+function validLength(length){
+    return length > 0;
+}
+
+function fillFirstRow(length){
+    for (i = 1; i <= length; i++){
+        matrix[currentPos.x][currentPos.y] = i;
+        let x = movement.currentMove[0];
+        let y = movement.currentMove[1];
+        currentPos.x += x;
+        currentPos.y += y;
+    }
+    movement.nextMove();
+}
+
+function updateResult(){
+    let length = matrixSize.value;
+    if (createSpiralMatrix(length)){
+        for (i = 0; i < length; i){
+            let row = document.createElement("div");
+            row.textContent = matrix[i];
+            result.appendChild(row);
+        }
+    }else{
+        result.textContent = "Invalid Entry";
+    }    
+}
