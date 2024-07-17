0faa0102f7b9facf99e428e6a00915f546a7d9a7
petery123 <139976625+petery123@users.noreply.github.com>
2024-07-17 17:34:52 +0100

    Create validate move function
    -It checks if the move about to be made is valid and returns true or
    false

--- help-matt.js
+++ help-matt.js
@@ -4,15 +4,17 @@ const resultBtn = document.querySelector("#resultBtn");
 resultBtn.addEventListener('click', updateResult);
 
 const MOVES = [
+    [1, 0], //E
+    [1, 1] //SE
     [0, 1], //S
     [-1, 1], //SW
     [-1, 0], //W
     [-1,-1], //NW
     [0, -1], //N
     [1, -1], //NE
-    [1, 0], //E
-    [1, 1] //SE
+    
 ]
+
 function validateInput(input){
     return Number.isInteger(Math.sqrt(input.split(",").length));
 }
@@ -31,6 +33,19 @@ function convertToMatrix(input){
         matrixMap.push(holder.slice());
     }
     return matrixMap;
+}
+
+function validateMove(currentPos, move, matrixMap){
+    nextPos = [currentPos[0] + move[0], currentPos[1] + move[1]]
+    maxIndex = matrixMap.length - 1;
+    if ((nextPos[0] <= maxIndex) && (nextPos[0] >= 0) && (nextPos[1] <= maxIndex) && (nextPos[1] >= 0)){ //checking if within bounds
+        let x = nextPos[0];
+        let y = nextPos[1];
+        if (matrixMap[x][y] === "d" || matrixMap[x][y] === "h"){
+            return true;
+        }
+    }
+    return false;
 }
 
 function updateResult(){
