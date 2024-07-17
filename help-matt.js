df944ed1e6920e572cc956e9364e2f1ff1f6cf38
petery123 <139976625+petery123@users.noreply.github.com>
2024-07-17 18:11:43 +0100

    Add main function to find the shortest route
    -It uses recursion to check every possible combinaiton

--- help-matt.js
+++ help-matt.js
@@ -15,11 +15,13 @@ const MOVES = [
     
 ]
 
+let shortestLen = 0;
+
 function validateInput(input){
     return Number.isInteger(Math.sqrt(input.split(",").length));
 }
 
-function convertToMatrix(input){
+function getMatrix(input){
     let matrixMap = [];
     let arr = input.split(",");
     let rowLength = Math.sqrt(arr.length);
@@ -46,6 +48,34 @@ function validateMove(currentPos, move, matrixMap){
         }
     }
     return false;
+}
+
+function nextMove(currentPos, matrixMap, movesLen){
+    let prevPos = currentPos;
+    for (i = 0; i < 8; i++){
+        let move = MOVES[i];
+        if (validateMove(currentPos, move, matrixMap)){
+            currentPos = [currentPos[0] + move[0], currentPos[1] + move[1]];
+            movesLen++;
+            if (matrixMap[currentPos[0]][currentPos[1]] === "d"){
+                matrixMap[currentPos[0]][currentPos[1]] = "m";
+                nextMove(currentPos, matrixMap, movesLen);
+            }else{
+                updateShortest(movesLen);
+                currentPos = prevPos;
+                movesLen--;
+            }
+        }else if (i === 7){
+            currentPos = prevPos;
+            movesLen--;
+        }
+    }
+}
+
+function updateShortest(movesLen){
+    if (movesLen < shortestLen){
+        shortestLen = movesLen;
+    }
 }
 
 function updateResult(){
