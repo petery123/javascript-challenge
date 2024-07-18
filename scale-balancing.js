1917e75b85fddd88328604a99dad0c2128275af2
petery123 <139976625+petery123@users.noreply.github.com>
2024-07-18 15:21:13 +0100

    Add element calls to js file

---
+++ scale-balancing.js
@@ -0,0 +1,4 @@
+const resultBtn =  document.querySelector("#resultBtn");
+const scaleMasses = document.querySelector("#scaleMasses");
+const weights = document.querySelector("#weights");
+const resultDisplay = document.querySelector(".resultDisplay");
