3499f308523b98172bea6b1da79bb308e9c0d71b
petery123 <139976625+petery123@users.noreply.github.com>
2024-07-18 15:39:22 +0100

    Add function to validate entries

--- scale-balancing.js
+++ scale-balancing.js
@@ -1,10 +1,37 @@
 const resultBtn =  document.querySelector("#resultBtn");
-const scaleMasses = document.querySelector("#scaleMasses");
-const weights = document.querySelector("#weights");
+const scaleMassesInput = document.querySelector("#scaleMasses");
+const weightsInput = document.querySelector("#weights");
 const resultDisplay = document.querySelector(".resultDisplay");
 
 resultBtn.addEventListener("click", updateResult);
 
-function updateResult(){
+function getData(){
+    let scaleMassArr = scaleMassesInput.textContent.split(",");
+    let weightsArr = weightsInput.textContent.split(",");
+    let data = [];
     
+    if (validateScaleMasses(scaleMassArr)){
+        data.push(scaleMassArr);
+        if (validateWeights(weightsArr)){
+            data.push(weightsArr);
+            return data;
+        }
+    }
+    return false;
+}
+
+function validateScaleMasses(scaleMassArr){
+    return scaleMassArr.length === 2;
+}
+
+function validateWeights(weightsArr){
+    return weightsArr.length >= 2;
+}
+
+function solve(){
+    
+}
+
+function updateResult(){
+    resultDisplay.textContent = "";
 }
