0a9bb0e589c3e9e7c481b2316a97b0045b1c317a
petery123 <139976625+petery123@users.noreply.github.com>
2024-07-18 15:49:28 +0100

    Implement way to check if one weight can solve

--- scale-balancing.js
+++ scale-balancing.js
@@ -9,7 +9,7 @@ function getData(){
     let scaleMassArr = scaleMassesInput.textContent.split(",");
     let weightsArr = weightsInput.textContent.split(",");
     let data = [];
-    
+
     if (validateScaleMasses(scaleMassArr)){
         data.push(scaleMassArr);
         if (validateWeights(weightsArr)){
@@ -29,7 +29,17 @@ function validateWeights(weightsArr){
 }
 
 function solve(){
-    
+    let data = getData();
+    if (data){
+        let max = data[0][0];
+        let min = data[0][1];
+        [max, min] = min > max ? [min, max] : [max, min];
+        let weights = data[1];
+        if (weights.includes(max-min)){//checks if a single mass could balance
+            return [max-min];
+        }
+    }
+    return false;
 }
 
 function updateResult(){
