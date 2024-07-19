39d3591a85204b7a7782e9a0a0841be90f2b986f
petery123 <139976625+petery123@users.noreply.github.com>
2024-07-19 19:55:31 +0100

    Add global variables to js file

---
+++ workout-timer.js
@@ -0,0 +1,10 @@
+const timerDisplay = document.querySelector(".timer");
+const continueBtn = document.querySelector("#continue");
+const pauseBtn = document.querySelector("#pause");
+const stopBtn = document.querySelector("#stopBtn");
+
+let timer = {
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+}
