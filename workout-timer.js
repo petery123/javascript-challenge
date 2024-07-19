2bb1f294e3aa90c0a0f8b5933b0aa2ba535c63b4
petery123 <139976625+petery123@users.noreply.github.com>
2024-07-19 20:05:11 +0100

    Add reset and addSecond function to timer

--- workout-timer.js
+++ workout-timer.js
@@ -7,4 +7,25 @@ let timer = {
     hours: 0,
     minutes: 0,
     seconds: 0,
+
+    reset: function(){
+        this.hours = 0;
+        this.minutes = 0;
+        this.seconds = 0;
+    },
+
+    addSecond: function(){
+        if (this.seconds < 59){
+            this.seconds++;
+            return
+        }
+        if (this.minutes < 59){
+            this.minutes++;
+            this.seconds = 0;
+            return
+        }
+        this.hours++;
+        this.minutes = 0;
+        this.seconds = 0;
+    }
 }
